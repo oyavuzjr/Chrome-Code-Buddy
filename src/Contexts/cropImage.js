acb123b21af030c450b88e1dbe47d9e5abe89899
oyavuzjr <oyavuz@g.ucla.edu>
2020-09-28 21:10:28 +0300

    made the preview of the screenshot half sized

--- src/Contexts/cropImage.js
+++ src/Contexts/cropImage.js
@@ -1,23 +1,23 @@
-export function cropImage (image64, pixelCrop) {
-    const canvas = document.createElement('canvas');
-    canvas.width = pixelCrop.width
-    canvas.height = pixelCrop.height
-    const ctx = canvas.getContext('2d')
-    const image = new Image()
-    image.src = image64
-    ctx.drawImage(
-        image,
-        pixelCrop.x,
-        pixelCrop.y,
-        pixelCrop.width,
-        pixelCrop.height,
-        0,
-        0,
-        pixelCrop.width,
-        pixelCrop.height)
-    // image.download = "snap-" + video.currentTime + ".png";
-    const url = canvas.toDataURL("image/jpeg", 1.0);
-    console.log(url)
-    return url
-
-  }
+export function cropImage(image64, pixelCrop) {
+  const canvas = document.createElement("canvas");
+  canvas.width = pixelCrop.width * 2;
+  canvas.height = pixelCrop.height * 2;
+  const ctx = canvas.getContext("2d");
+  const image = new Image();
+  image.src = image64;
+  ctx.drawImage(
+    image,
+    pixelCrop.x * 2,
+    pixelCrop.y * 2,
+    pixelCrop.width * 2,
+    pixelCrop.height * 2,
+    0,
+    0,
+    pixelCrop.width * 2,
+    pixelCrop.height * 2
+  );
+  // image.download = "snap-" + video.currentTime + ".png";
+  const url = canvas.toDataURL("image/jpeg", 1.0);
+  console.log(url);
+  return url;
+}
